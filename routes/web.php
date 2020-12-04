6c74a300a8357a3932b2f180c8199d465b6323be
Rahul <rahular60@gmail.com>
2020-12-04 06:34:23 +0530

    added routes

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,9 @@ Route::get('/posts/{id}','PostsController@index');
 
 
 Route::get('/articles','ArticlesController@index');
+Route::get('/articles/create','ArticlesController@create');
+Route::post('/articles','ArticlesController@store');
+Route::get('/articles/{article}','ArticlesController@show');
+Route::get('/articles/{article}/edit','ArticlesController@edit');
+Route::put('/articles/{article}','ArticlesController@update');
+Route::delete('/articles/{article}','ArticlesController@destroy');
